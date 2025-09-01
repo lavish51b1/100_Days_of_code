9ed9c2430d920c7c1c2fc369eb5ac5d5abeec6e2
lavish51b1 <lavishgoyal0209@gmail.com>
2025-09-02 00:06:34 +0530

    Initial commit

---
+++ Day1/Q1.c
@@ -0,0 +1,18 @@
+ # include<stdio.h>
+  
+  int main() {
+   
+  int a, b;
+  
+  printf("first number");
+  scanf("%d",&a);
+ 
+  printf("second number");
+  scanf("%d",&b);
+ 
+  int sum = a+b;
+ 
+  printf("sum of these two numbers:%d", sum);
+
+  return 0;
+ }
